27447a6c27821b191b2f2bfb0d69dae0e78a64c2
Paulo Henrique Lopes <55982445+pauloholiver@users.noreply.github.com>
2021-07-01 09:01:53 -0300

    semi-final adaptations

---
+++ Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace CrudInstrumentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.lblCod = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dtInstrumentos = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtInstrumentos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(258, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cadastro de Instrumentos";
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPreco.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblPreco.Location = new System.Drawing.Point(138, 221);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(72, 30);
+            this.lblPreco.TabIndex = 1;
+            this.lblPreco.Text = "Preço";
+            // 
+            // lblCod
+            // 
+            this.lblCod.AutoSize = true;
+            this.lblCod.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCod.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblCod.Location = new System.Drawing.Point(123, 105);
+            this.lblCod.Name = "lblCod";
+            this.lblCod.Size = new System.Drawing.Size(87, 30);
+            this.lblCod.TabIndex = 1;
+            this.lblCod.Text = "Codigo";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDesc.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblDesc.Location = new System.Drawing.Point(97, 162);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(113, 30);
+            this.lblDesc.TabIndex = 2;
+            this.lblDesc.Text = "Descrição";
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblUrl.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblUrl.Location = new System.Drawing.Point(65, 275);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(145, 30);
+            this.lblUrl.TabIndex = 3;
+            this.lblUrl.Text = "URL Imagem";
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(236, 101);
+            this.txtCod.Multiline = true;
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(526, 34);
+            this.txtCod.TabIndex = 4;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(236, 275);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(526, 79);
+            this.txtUrl.TabIndex = 7;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(236, 158);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(526, 34);
+            this.txtDesc.TabIndex = 8;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(236, 217);
+            this.txtPreco.Multiline = true;
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(526, 34);
+            this.txtPreco.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Purple;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(236, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 36);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Cadastrar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnCadastrar);
+            // 
+            // dtInstrumentos
+            // 
+            this.dtInstrumentos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtInstrumentos.BackgroundColor = System.Drawing.Color.White;
+            this.dtInstrumentos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtInstrumentos.Location = new System.Drawing.Point(48, 433);
+            this.dtInstrumentos.MultiSelect = false;
+            this.dtInstrumentos.Name = "dtInstrumentos";
+            this.dtInstrumentos.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dtInstrumentos.RowTemplate.Height = 25;
+            this.dtInstrumentos.RowTemplate.ReadOnly = true;
+            this.dtInstrumentos.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtInstrumentos.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dtInstrumentos.Size = new System.Drawing.Size(848, 200);
+            this.dtInstrumentos.TabIndex = 11;
+            this.dtInstrumentos.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dtInstrumentos_CellMouseDoubleClick);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.Purple;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnBuscar.Location = new System.Drawing.Point(795, 101);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(101, 36);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.BackColor = System.Drawing.Color.Purple;
+            this.btnAlterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAlterar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAlterar.Location = new System.Drawing.Point(442, 376);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(121, 36);
+            this.btnAlterar.TabIndex = 12;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = false;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.BackColor = System.Drawing.Color.Purple;
+            this.btnDeletar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnDeletar.ForeColor = System.Drawing.Color.White;
+            this.btnDeletar.Location = new System.Drawing.Point(641, 376);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(121, 36);
+            this.btnDeletar.TabIndex = 13;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = false;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(940, 645);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dtInstrumentos);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.lblUrl);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblCod);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtInstrumentos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.Label lblCod;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dtInstrumentos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnDeletar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrudInstrumentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection("Data Source=PAULINHO;Initial Catalog=ApiInstrumentos;User ID=sa;Password=phflo0905");
+
+        private void btnCadastrar(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand("exec dbo.Instrumento_Insert '" + txtDesc.Text + "','" + decimal.Parse(txtPreco.Text) + "','" + txtUrl.Text + "'", con);
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Inserido novo instrumento com sucesso");
+            CarregaInstrumentos();
+        }
+
+        void CarregaInstrumentos()
+        {
+            SqlCommand com = new SqlCommand("exec dbo.Instrumento_View", con);
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable instrumentos = new DataTable();
+            da.Fill(instrumentos);
+            dtInstrumentos.DataSource = instrumentos;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CarregaInstrumentos();
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand com = new SqlCommand("exec dbo.Instrumento_Update '" + txtDesc.Text + "','" + decimal.Parse(txtPreco.Text) + "','" + txtUrl.Text + "'", con);
+            com.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Instrumento alterado com sucesso");
+            CarregaInstrumentos();
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Quer realmente excluir este instrumento?", "Excluir", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand("exec dbo.Instrumento_Delete '" + int.Parse(txtCod.Text) + "'", con);
+                com.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Instrumento excluído com sucesso");
+                CarregaInstrumentos();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (txtCod.Text != "")
+            {
+                SqlCommand com = new SqlCommand("exec dbo.Instrumento_Search'" + int.Parse(txtCod.Text) + "'", con);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                DataTable instrumentos = new DataTable();
+                da.Fill(instrumentos);
+                dtInstrumentos.DataSource = instrumentos;
+            }
+            else
+                CarregaInstrumentos();
+        }
+
+        private void dtInstrumentos_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+                txtCod.Text = this.dtInstrumentos.CurrentRow.Cells[0].Value.ToString();
+                txtDesc.Text = this.dtInstrumentos.CurrentRow.Cells[1].Value.ToString();
+                txtPreco.Text = this.dtInstrumentos.CurrentRow.Cells[2].Value.ToString();
+                txtUrl.Text = this.dtInstrumentos.CurrentRow.Cells[3].Value.ToString();
+        }
+    }
+}
